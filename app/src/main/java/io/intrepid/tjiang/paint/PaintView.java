ec981ca091c9168224ce77c0ab22aa0db87dc942
Tiancheng Jiang <tjiang@Tianchengs-MacBook-Pro.local>
2015-06-10 15:43:48 -0400

    Initialize Paint object to Blue

--- app/src/main/java/io/intrepid/tjiang/paint/PaintView.java
+++ app/src/main/java/io/intrepid/tjiang/paint/PaintView.java
@@ -3,6 +3,8 @@ package io.intrepid.tjiang.paint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -15,6 +17,8 @@ public class PaintView extends View {
 
     Bitmap b = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
     Canvas c = new Canvas(b);
+    //init paint to blue
+    Paint testPaint = initPaint();
 
     public PaintView(Context context) {
         super(context);
@@ -28,9 +32,13 @@ public class PaintView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @Override
     public boolean onTouchEvent(MotionEvent event){
         float x = event.getX();
         float y = event.getY();
+
+
+
         return true;
     }
 
@@ -38,6 +46,12 @@ public class PaintView extends View {
     protected void onDraw(Canvas canvas) {
 
     }*/
+
+    private Paint initPaint() {
+        Paint testPaint = new Paint();
+        testPaint.setColor(Color.BLUE);
+        return testPaint;
+    }
 
 
 
